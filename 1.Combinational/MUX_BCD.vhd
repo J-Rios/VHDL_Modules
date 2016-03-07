b69e2c5d4039cb5f95ee6597aa84c4c1214b69a6
Jose <jrios.rubio23@gmail.com>
2016-03-07 12:53:46 +0100

    Update MUX_BCD.vhd

--- 1.Combinational/MUX_BCD.vhd
+++ 1.Combinational/MUX_BCD.vhd
@@ -9,9 +9,9 @@ entity MUX_BCD is
     (
         A : in  STD_LOGIC_VECTOR (3 downto 0);
         B : in  STD_LOGIC_VECTOR (3 downto 0);
-		C : in  STD_LOGIC_VECTOR (3 downto 0);
+	C : in  STD_LOGIC_VECTOR (3 downto 0);
         D : in  STD_LOGIC_VECTOR (3 downto 0);
-		S : in  STD_LOGIC_VECTOR (1 downto 0);
+	S : in  STD_LOGIC_VECTOR (1 downto 0);
         Z : out STD_LOGIC_VECTOR (3 downto 0)
     );
 end MUX_BCD;
@@ -22,18 +22,18 @@ architecture Behavioral of MUX_BCD is
 
 begin
 
-	-- One option
-	Z <= A when S = "00" else
+    -- One option
+    Z <= A when S = "00" else
          B when S = "01" else
-		 C when S = "10" else
-		 D when S = "11";
+ 	 C when S = "10" else
+	 D when S = "11";
 	
     -- Other option
-    -- with S select
-       -- Z <= A when "00",
-            -- B when "01",
-			-- C when "10",
-			-- D when "11",
-            -- D when others; -- Dummy (Needed for simulation)
+    -- with S select Z <= 
+          -- A when "00",
+          -- B when "01",
+	  -- C when "10",
+	  -- D when "11",
+          -- D when others; -- Dummy (Needed for simulation)
 
 end Behavioral;
