d0a59ce221dd1617659510568298723a75a23c65
Jose <jrios.rubio23@gmail.com>
2016-03-07 13:03:58 +0100

    Update Counter_updown.vhd

--- 2.Secuencial/Counter_updown.vhd
+++ 2.Secuencial/Counter_updown.vhd
@@ -6,35 +6,35 @@ use IEEE.NUMERIC_STD.ALL;
 -------------------------------------------------------------------------
 
 entity COUNTER_UpDw is
-	Generic
-	(
-		BITS : INTEGER := 4
-	);
+    Generic
+    (
+	BITS : INTEGER := 4
+    );
     Port
     (
-		CLK	: in  	STD_LOGIC;
-        UP  : in	STD_LOGIC;
-		Q   : inout STD_LOGIC_VECTOR (BITS-1 downto 0)
+	CLK : in    STD_LOGIC;
+        UP  : in    STD_LOGIC;
+	Q   : inout STD_LOGIC_VECTOR (BITS-1 downto 0)
     );
 end COUNTER_UpDw;
 
 -------------------------------------------------------------------------
 
 architecture Behavioral of COUNTER_UpDw is
-	signal Count : UNSIGNED (BITS-1 downto 0) := (others => '0');
+    signal Count : UNSIGNED (BITS-1 downto 0) := (others => '0');
 begin
 	
-	process(UP, CLK)
-	begin
-	    if (CLK'event and CLK = '1') then
-	        if (UP = '1') then
-				Count <= Count + 1;
-			else
-				Count <= Count - 1;
-			end if;
+    process(UP, CLK)
+    begin
+        if (CLK'event and CLK = '1') then
+            if (UP = '1') then
+    	        Count <= Count + 1;
+	    else
+		Count <= Count - 1;
+	    end if;
         end if;
     end process;
 
-	Q <= std_logic_vector(Count);
+    Q <= std_logic_vector(Count);
 
 end Behavioral;
