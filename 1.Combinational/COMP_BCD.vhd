2c51d25d71fe73c3c09e7bd0f5948c30967a9767
Jose <jrios.rubio23@gmail.com>
2016-03-07 12:46:06 +0100

    Update COMP_BCD.vhd

--- 1.Combinational/COMP_BCD.vhd
+++ 1.Combinational/COMP_BCD.vhd
@@ -12,7 +12,6 @@
 --                                                                              --
 ----------------------------------------------------------------------------------
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -21,11 +20,11 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity COMP_BCD is 
     port
     (
-        A     		: in  STD_LOGIC_VECTOR (3 downto 0);
-		B     		: in  STD_LOGIC_VECTOR (3 downto 0);
-		AminusB     : out STD_LOGIC;
-        AequalB     : out STD_LOGIC;
-		AmajorB     : out STD_LOGIC
+        A     	: in  STD_LOGIC_VECTOR (3 downto 0);
+	B       : in  STD_LOGIC_VECTOR (3 downto 0);
+	AminusB : out STD_LOGIC;
+        AequalB : out STD_LOGIC;
+	AmajorB : out STD_LOGIC
     );
 end COMP_BCD;
 
@@ -33,8 +32,8 @@ architecture BEHAVIORAL of COMP_BCD is
 begin
 
     AminusB <= '1' when (A < B) else '0';
-	AequalB <= '1' when (A = B) else '0';
-	AmajorB <= '1' when (A > B) else '0';
+    AequalB <= '1' when (A = B) else '0';
+    AmajorB <= '1' when (A > B) else '0';
 	
 end BEHAVIORAL;
 
