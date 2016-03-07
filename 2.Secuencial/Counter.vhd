69ddfed5f2a49b391cf353fe05ed9bb14be2ed1b
Jose <jrios.rubio23@gmail.com>
2016-03-07 12:58:20 +0100

    Update Counter.vhd

--- 2.Secuencial/Counter.vhd
+++ 2.Secuencial/Counter.vhd
@@ -6,31 +6,31 @@ use IEEE.NUMERIC_STD.ALL;
 -------------------------------------------------------------------------
 
 entity COUNTER is
-	Generic
-	(
-		BITS : INTEGER := 4
-	);
+    Generic
+    (
+        BITS : INTEGER := 4
+    );
     Port
     (
-		CLK	: in  STD_LOGIC;
+	CLK : in  STD_LOGIC;
         RST : in STD_LOGIC;
-		CE  : in STD_LOGIC;
-		Q   : out STD_LOGIC_VECTOR (BITS-1 downto 0);
-		TC  : out STD_LOGIC
+	CE  : in STD_LOGIC;
+	TC  : out STD_LOGIC;
+	Q   : out STD_LOGIC_VECTOR (BITS-1 downto 0)
     );
 end COUNTER;
 
 -------------------------------------------------------------------------
 
 architecture Behavioral of COUNTER is
-	signal Qr : UNSIGNED (BITS-1 downto 0);
-	signal Qn : UNSIGNED (BITS-1 downto 0);
+    signal Qr : UNSIGNED (BITS-1 downto 0);
+    signal Qn : UNSIGNED (BITS-1 downto 0);
 begin
 	
-	process(RST, CLK, CE)
-	begin
-	    if (CLK'event and CLK = '1') then -- Rising clock
-	        if (CE = '1') then -- Chip enable active
+    process(RST, CLK, CE)
+    begin
+        if (CLK'event and CLK = '1') then -- Rising clock
+            if (CE = '1') then -- Chip enable active
                 if (RST = '1') then -- Reset active
                     Qr <= (others => '0'); -- Reset the count to zero
                 else
@@ -40,11 +40,11 @@ begin
         end if;
     end process;
 	
-	-- Next state logic
-	Qn <= Qr + 1; -- Increase the count
+    -- Next state logic
+    Qn <= Qr + 1; -- Increase the count
 	
-	-- Output logic
-	Q <= std_logic_vector(Qr); -- Output vector
-	TC <= '1' when Qr = (2**BITS-1) else '0'; -- Tick-Count bit (End-count)
+    -- Output logic
+    Q <= std_logic_vector(Qr); -- Output vector
+    TC <= '1' when Qr = (2**BITS-1) else '0'; -- Tick-Count bit (End-count)
 
 end Behavioral;
