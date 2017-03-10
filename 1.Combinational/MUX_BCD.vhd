0d3cdd660b988b6a40dff05f30ecef7e079e173f
Jose <jrios.rubio23@gmail.com>
2017-03-10 16:06:43 +0100

    Update MUX_BCD.vhd

--- 1.Combinational/MUX_BCD.vhd
+++ 1.Combinational/MUX_BCD.vhd
@@ -1,25 +1,40 @@
+----------------------------------------------------------------------------------
+--                           -------------------                                --
+--                          |                   |                               --
+--     A[BITS-1:0] ---------| A                 |                               --
+--     B[BITS-1:0] ---------| B                 |                               --
+--     C[BITS-1:0] ---------| C               Z |--------- Z[BITS-1:0]          --
+--     D[BITS-1:0] ---------| D                 |                               --
+--                          |                   |                               --
+--          S[1:0] ---------| S                 |                               --
+--                          |                   |                               --
+--                           -------------------                                --
+----------------------------------------------------------------------------------
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--------------------------------------------------------------------------
+----------------------------------------------------------------------------------
 
 entity MUX_BCD is
+    generic
+    (
+        BITS : INTEGER := 4
+    );
     Port
     (
-        A : in  STD_LOGIC_VECTOR (3 downto 0);
-        B : in  STD_LOGIC_VECTOR (3 downto 0);
-	C : in  STD_LOGIC_VECTOR (3 downto 0);
-        D : in  STD_LOGIC_VECTOR (3 downto 0);
+        A : in  STD_LOGIC_VECTOR (BITS-1 downto 0);
+        B : in  STD_LOGIC_VECTOR (BITS-1 downto 0);
+	C : in  STD_LOGIC_VECTOR (BITS-1 downto 0);
+        D : in  STD_LOGIC_VECTOR (BITS-1 downto 0);
 	S : in  STD_LOGIC_VECTOR (1 downto 0);
-        Z : out STD_LOGIC_VECTOR (3 downto 0)
+        Z : out STD_LOGIC_VECTOR (BITS-1 downto 0)
     );
 end MUX_BCD;
 
--------------------------------------------------------------------------
+----------------------------------------------------------------------------------
 
 architecture Behavioral of MUX_BCD is
-
 begin
 
     -- One option
