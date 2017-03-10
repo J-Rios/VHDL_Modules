fb3c0a7552c0419b5774359f5e8247989332d1d8
Jose <jrios.rubio23@gmail.com>
2017-03-10 15:50:58 +0100

    Update H_Adder_BCD.vhd

--- 1.Combinational/H_Adder_BCD.vhd
+++ 1.Combinational/H_Adder_BCD.vhd
@@ -1,20 +1,33 @@
+----------------------------------------------------------------------------------
+--                           -------------------                                --
+--                          |                   |                               --
+--     A[BITS-1:0] ---------| A                 |                               --
+--                          |                 Z |--------- Z[BITS-1:0]          --
+--     B[BITS-1:0] ---------| B                 |                               --
+--                          |                   |                               --
+--                           -------------------                                --
+----------------------------------------------------------------------------------
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
--------------------------------------------------------------------------
+----------------------------------------------------------------------------------
 
 entity H_Adder_BCD is
+    generic
+    (
+        BITS : INTEGER := 4
+    );
     Port
     (
-        A : in  STD_LOGIC_VECTOR (3 downto 0);
-	B : in  STD_LOGIC_VECTOR (3 downto 0);
-	Z : out STD_LOGIC_VECTOR (3 downto 0)
+        A : in  STD_LOGIC_VECTOR (BITS-1 downto 0);
+	B : in  STD_LOGIC_VECTOR (BITS-1 downto 0);
+	Z : out STD_LOGIC_VECTOR (BITS-1 downto 0)
     );
 end H_Adder_BCD;
 
--------------------------------------------------------------------------
+----------------------------------------------------------------------------------
 
 architecture Behavioral of H_Adder_BCD is
     signal Sum : UNSIGNED (3 downto 0);
